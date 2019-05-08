b251a773e559b733942d153cd7fe66c010f55ca5
keeng0903 <phu.tran170204@vnuk.edu.vn>
2019-05-08 08:43:49 +0700

    first commit

---
+++ src/main/java/vn/edu/vnuk/airlines/VnukAirlinesApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.vnuk.airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnukAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnukAirlinesApplication.class, args);
+	}
+
+}
